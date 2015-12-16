d582b639b4426972db42ea2a367c0aec48ede7e7
Mark Knol <mark.knol@gmail.com>
2015-12-16 20:50:04 +0100

    Update Jsonp.hx
    Consistent spaces and comment endings

--- src/haxe/Jsonp.hx
+++ src/haxe/Jsonp.hx
@@ -8,44 +8,45 @@ import js.Browser.document;
   to be retrieved from systems other than the one the page was served by.
   
   @author Mark Knol
- */
+**/
 class Jsonp {
   static private var _ID = 0;
   
   /**
-    Convenience method to create a new Jsonp instance with given callback information.
-  */
+    Convenience method to create a new Jsonp instance with given callback 
+    information.
+  **/
   public static function requestUrl<T>(url:String, callback:T->Void, jsonCallbackName:String = null) {
-    var j = new JsonP(url);
+    var j = new Jsonp(url);
     j.onData = callback;
     j.request(jsonCallbackName);
     return j;
   }
   
-	/**
-		The url of `this` request. It is used only by the request() method and
-		can be changed in order to send the same request to different target
-		Urls.
-	**/
+  /**
+    The url of `this` request. It is used only by the request() method and
+    can be changed in order to send the same request to different target
+    Urls.
+  **/
   public var url:String;
   
   /**
-		This method is called upon a successful request, with `data` containing
-		the result object.
-
-		The intended usage is to bind it to a custom function:
-		`JsonpInstance.onData = function(data) { // handle result }`
-	**/
+    This method is called upon a successful request, with `data` containing
+    the result object.
+    
+    The intended usage is to bind it to a custom function:
+    `JsonpInstance.onData = function(data) { // handle result }`
+  **/
   public var onData:Dynamic->Void;
   
   /**
-		Creates a new JsonP instance with `url` as parameter.
-
-		This does not do a request until request() is called.
-
-		If `url` is null, the field url must be set to a value before making the
-		call to request(), or the result is unspecified.
-	**/
+    Creates a new JsonP instance with `url` as parameter.
+    
+    This does not do a request until request() is called.
+    
+    If `url` is null, the field url must be set to a value before making the
+    call to request(), or the result is unspecified.
+  **/
   public function new(url:String = "") {
     this.url = url;
   }
@@ -61,7 +62,7 @@ class Jsonp {
     
     @param callbackName (optional) can be used to set a fixed callback
                         name, otherwise unique name is generated.
-  */
+  **/
   public function request(callbackName:String = null) {
     var name =  callbackName != null ? callbackName : "unique_callback_" + _ID++;
     if (url.indexOf('?') != -1) url += "&callback=" + name;
